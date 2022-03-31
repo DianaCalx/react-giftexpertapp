e7ca9e76b71918b318a70990968266cf25e7e820
Diana Calderon <calderon.calx@gmai.com>
2022-03-30 19:19:28 -0600

    docs agregado

---
+++ docs/precache-manifest.e840719aa8636a98bdcdb2d12b1a43bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adb1a548c77359cefd98f4b5c1e8f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81ee7d202214db018232",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ee7d202214db018232",
+    "url": "/static/js/main.b387c200.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
